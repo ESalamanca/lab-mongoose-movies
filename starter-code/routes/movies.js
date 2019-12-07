2463d3e8a23c27a91deb58ba12e2c3f37ac8b772
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-12-07 17:46:27 +0100

    Iteration 12 done

--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -5,6 +5,7 @@ const Movie = require('../models/movie.js');
 const Celebrity=require('../models/celebrity.js');
 
 router.get('/new', (req,res,next)=>{
+  //need to fetch the celebrities in our dB in order to give choice for casting of new movie
   Celebrity.find().then(celebrities=>{
     res.render('movies/new', {celebrities});
   }).catch(err=>{
@@ -16,7 +17,8 @@ router.get('/new', (req,res,next)=>{
 }); 
 
 router.post('/', (req,res,next)=>{
-  const { title, genre, plot, cast }= req.body; 
+  const { title, genre, plot, cast }= req.body;
+  //Creation of new movie in DB
   Movie.create({title,genre,plot,cast})
   .then(()=>{
     res.redirect('/movies');
@@ -39,6 +41,55 @@ router.post('/:movie_id/delete', (req,res,next)=>{
   Movie.findByIdAndDelete(req.params.movie_id).then(()=>{
     res.redirect('/movies');
   }).catch(err=>{
+    console.error(err);
+    next(err);
+  })
+});
+
+router.get('/:movie_id/edit', (req,res,next)=>{
+  //Retrieve the movie information 
+  Movie.findById(req.params.movie_id).populate('cast').then(movie=>{
+    Celebrity.find().then(celebrities =>{
+      const newCelebs = celebrities.map(celeb => ({
+        id: celeb._id,
+        name: celeb.name,
+        inMovie: false,
+      }));
+      movie.cast=movie.cast.map(celeb=>{return celeb._id});
+      newCelebs.forEach(celeb=>{
+        if (movie.cast.indexOf(celeb.id)>-1) {
+          celeb.inMovie=true;
+        }
+      })
+
+      res.render('movies/edit', {
+        movie: movie,
+        newCelebs: newCelebs
+      });
+
+      //res.send({movie,newCelebs});
+
+    }).catch(err=>{
+      console.error(err);
+      next(err);
+    });    
+  }).catch(err=>{
+    console.error(err);
+    next(err);
+  });
+});
+
+router.post('/:movie_id', (req,res,next)=>{
+  const {title, genre, plot, cast}=req.body;
+  Movie.updateOne({_id: req.params.movie_id}, {$set:{
+    title,
+    genre,
+    plot,
+    cast
+  }}).then(
+    res.redirect(`/movies/${req.params.movie_id}`)
+
+  ).catch(err=>{
     console.error(err);
     next(err);
   })
