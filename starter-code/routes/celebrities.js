2c636864d953491cf9020607b2452116c070127c
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-12-07 12:47:12 +0100

    Iteration 3 done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -7,16 +7,24 @@ const Celebrity = require('../models/celebrity.js');
 router.get('/', (req, res, next) => {
 
   Celebrity.find({},null, {sort: { name: 1 }}).then(celebrities=>{
-
     res.render('celebrities/index', {celebrities});
-
   }).catch(err=>{
 
     console.error(err);
     next(err);
-  })
-
-  
+  })  
 });
+
+router.get('/:celebrity_id', (req,res,next)=>{
+  Celebrity.findById(req.params.celebrity_id).then(celebrity=>{
+
+    res.render('celebrities/show',{celebrity}); 
+
+  }).catch(err=>{
+    console.error(err);
+    next(err);
+  })
+  
+})
 
 module.exports = router;
